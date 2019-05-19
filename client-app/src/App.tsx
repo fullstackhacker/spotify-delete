9b918340100313a7776ef28773171c1bd3eb2189
kapadiamush <kapadiamush@gmail.com>
2019-05-18 17:01:46 -0700

    login with spotify button

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+  const login = useCallback(() => {
+    window.location.href = '/api/auth/';
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={login}> Login </button>
     </div>
   );
-}
+};
 
 export default App;
